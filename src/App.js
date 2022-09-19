efcab5785fd661f2397338069168475b6f4542fd
RuhamaMalik <83574511+RuhamaMalik@users.noreply.github.com>
2022-09-19 12:00:13 +0500

    ....

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import "./styles.css";
+import ReactDOM from "react-dom";
+import React, { useState } from 'react' ;
 
 export default function App() {
+  
+
+    const [num , setNum] = useState(5);
+  const decrease = ()=>{
+    if(num === 0){
+      alert("The number cannot be less than 0 ");
+    }else{
+      setNum(num-1);
+    }
+    
+    }
+  const increase = ()=>{
+    setNum(num+1)
+    }
+     const reset = ()=>{
+       setNum(0);
+       }
+  
   return (
+    <>
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="parent" >
+        <h1> {num}</h1>
+        <div>
+        <button onClick={decrease} >-</button>
+        <button onClick={increase} >+</button ><br />
+        <button onClick={reset} >Reset</button >
+      </div>
+        </div>
     </div>
+      </>
   );
 }
